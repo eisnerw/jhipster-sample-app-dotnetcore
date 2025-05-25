1ba822dd80c7fa27099f0c96fe52e0ab52272fe6
Bill Eisner <eisnerw@hotmail.com>
2025-05-25 22:02:35 +0000

    Fix named query ui for delete

--- src/JhipsterSampleApplication/ClientApp/src/app/entities/named-query/delete/named-query-delete-dialog.component.ts
+++ src/JhipsterSampleApplication/ClientApp/src/app/entities/named-query/delete/named-query-delete-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule } from '@angular/forms';
 
 import SharedModule from 'app/shared/shared.module';
 import { ITEM_DELETED_EVENT } from 'app/config/navigation.constants';
@@ -11,7 +12,7 @@ import { NamedQueryService } from '../service/named-query.service';
 @Component({
   standalone: true,
   templateUrl: './named-query-delete-dialog.component.html',
-  imports: [CommonModule, FontAwesomeModule, SharedModule],
+  imports: [CommonModule, FontAwesomeModule, SharedModule, FormsModule],
 })
 export class NamedQueryDeleteDialogComponent {
   namedQuery?: INamedQuery;
