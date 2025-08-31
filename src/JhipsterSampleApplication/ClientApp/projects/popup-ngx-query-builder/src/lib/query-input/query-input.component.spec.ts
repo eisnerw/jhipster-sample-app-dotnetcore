31fcad70793199b8c508b4681c81c25a2da8509b
eisnerw <eisnerw@hotmail.com>
2025-08-31 22:05:38 +0000

    fix(named-query): save query text instead of name

--- src/JhipsterSampleApplication/ClientApp/projects/popup-ngx-query-builder/src/lib/query-input/query-input.component.spec.ts
+++ src/JhipsterSampleApplication/ClientApp/projects/popup-ngx-query-builder/src/lib/query-input/query-input.component.spec.ts
@@ -1,14 +1,30 @@
 import { QueryInputComponent } from './query-input.component';
-import { MatDialog } from '@angular/material/dialog';
 import { RuleSet, Rule } from 'ngx-query-builder';
 
 describe('QueryInputComponent', () => {
   it('should set default operator when parsing empty query', () => {
-    const component = new QueryInputComponent({} as MatDialog);
+    const component = new QueryInputComponent();
     component.defaultRuleAttribute = 'document';
     const rs: RuleSet = component.parseQuery('');
     const rule = rs.rules[0] as Rule;
     expect(rule.field).toBe('document');
     expect(rule.operator).toBe('contains');
+  });
+
+  it('should save named ruleset text instead of name', () => {
+    const component = new QueryInputComponent();
+    (component as any).config = { fields: {} } as any;
+    const postSpy = jasmine
+      .createSpy('post')
+      .and.returnValue({ subscribe: () => {} } as any);
+    (component as any).http = { post: postSpy, put: jasmine.createSpy('put') };
+    const rs: RuleSet = {
+      condition: 'and',
+      rules: [{ field: 'a', operator: '=', value: 1 }],
+      name: 'TEST',
+    };
+    component.saveNamedRuleset(rs);
+    const payload = postSpy.calls.mostRecent().args[1];
+    expect(payload.text).toBe('a=1');
   });
 });
